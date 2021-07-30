a7e6f6f59dc8fdaa749aee71db07bd5eb1fa7834
MUmerYasin <mumeryasin123456789@gmail.com>
2021-07-30 05:31:01 -0700

    Flutter: Simple Drop Down Button and Custom Drop Down Button that change Circle Avatar and text when we click on it

--- lib/main.dart
+++ lib/main.dart
@@ -12,7 +12,6 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-
         primarySwatch: Colors.blue,
       ),
       home: MyHomePage(title: 'Flutter Demo Home Page'),
@@ -23,8 +22,6 @@ class MyApp extends StatelessWidget {
 class MyHomePage extends StatefulWidget {
   MyHomePage({Key? key, required this.title}) : super(key: key);
 
-
-
   final String title;
 
   @override
@@ -32,31 +29,28 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-
   String dropdownValue = 'One';
+
+  String secondDropDownValue = 'Apple';
+  var items = ['Apple','Banana','Grapes', 'Orange','watermelon'];
 
   @override
   Widget build(BuildContext context) {
-
     return Scaffold(
       appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
+
         title: Text(widget.title),
       ),
-      body: Center(
-
+      body: SingleChildScrollView(
         child: Column(
-
           mainAxisAlignment: MainAxisAlignment.start,
           crossAxisAlignment: CrossAxisAlignment.center,
           children: <Widget>[
-
             Padding(
               padding: const EdgeInsets.all(16.0),
               child: Text(
-                'CircleAvatar:',
-                style: TextStyle( fontSize: 30.0),
+                'Circle Avatar:',
+                style: TextStyle(fontSize: 30.0),
               ),
             ),
             Padding(
@@ -70,31 +64,35 @@ class _MyHomePageState extends State<MyHomePage> {
                     ),
                   ),
                   Expanded(
-                    child: Text('M. Umer Yasin', style: TextStyle(fontSize: 15.0),),
+                    child: Text(
+                      'M. Umer Yasin',
+                      style: TextStyle(fontSize: 15.0),
+                    ),
                   ),
                 ],
               ),
             ),
-
             const Divider(
               height: 10,
               thickness: 5,
               indent: 20,
               endIndent: 20,
             ),
-
             Padding(
               padding: const EdgeInsets.all(8.0),
               child: Row(
                 children: [
                   Expanded(
-                    child:  CircleAvatar(
+                    child: CircleAvatar(
                       backgroundColor: Colors.brown.shade800,
                       child: const Text('AH'),
                     ),
                   ),
                   Expanded(
-                    child: Text('M. Umer Yasin', style: TextStyle(fontSize: 15.0),),
+                    child: Text(
+                      'Umer Yasin',
+                      style: TextStyle(fontSize: 15.0),
+                    ),
                   ),
                 ],
               ),
@@ -102,21 +100,59 @@ class _MyHomePageState extends State<MyHomePage> {
 
             Padding(
               padding: const EdgeInsets.all(16.0),
+              child: Text(
+                'Drop down Button:',
+                style: TextStyle(fontSize: 30.0),
+              ),
+            ),
+
+            Padding(
+              padding: const EdgeInsets.all(8.0),
               child: Row(
                 children: [
-                  
+
+                  Expanded(
+                    child: DropdownButton(
+                      value: secondDropDownValue,
+
+                      items: items.map((itemsname) {
+                        return DropdownMenuItem(
+                          value: itemsname,
+                          child: Text(itemsname),
+                        );
+                      }).toList(),
+
+                      onChanged: (String? newValue) {
+                        setState(() {
+                          secondDropDownValue = newValue!;
+                        });
+                      },
+
+                    ),
+                  ),
+                ],
+
+              ),
+            ),
+
+            Divider(),
+            
+            Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: Row(
+                children: [
                   Expanded(
                     child: DropdownButton<String>(
                       value: dropdownValue,
                       icon: const Icon(Icons.arrow_downward),
                       iconSize: 24,
                       elevation: 24,
-                      style: const TextStyle(color: Colors.deepPurple),
+                      style: const TextStyle(
+                          color: Colors.deepPurple, fontSize: 20.0),
                       underline: Container(
-                        height: 2,
-                        color: Colors.black,
+                        height: 3,
+                        color: Colors.grey[300],
                       ),
-
 
                       onChanged: (String? newValue) {
                         setState(() {
@@ -130,17 +166,45 @@ class _MyHomePageState extends State<MyHomePage> {
                           value: value,
                           child: Text(value),
                         );
+
                       }).toList(),
                     ),
                   ),
                 ],
               ),
             ),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Row(
+                children: [
+                  Expanded(
+                    child: Text(
+                      " You Selected : ",
+                      style: TextStyle(fontSize: 20.0),
+                    ),
+                  ),
+                  Expanded(
+                    child: CircleAvatar(
+                      backgroundColor: Colors.purple.shade800,
+                      child: Text(dropdownValue),
+                    ),
+                  ),
+                  Expanded(
+                    child: Text(
+                      " : " + dropdownValue,
+                      style: TextStyle(fontSize: 20.0),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+
+
           ],
         ),
       ),
 
-    //   ), // This trailing comma makes auto-formatting nicer for build methods.
+      //   ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
